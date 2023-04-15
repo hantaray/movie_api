f3c2d6a6e837ef0be9700b7c1025356727c30409
hansashi <ahantke@mail.de>
2023-04-15 13:10:12 +0200

    updated topMovies

--- index.js
+++ index.js
@@ -5,44 +5,164 @@ const app = express();
 
 let topMovies = [
     {
+        id: 1,
+        title: "Oldboy",
+        description: "...",
+        genre: "thriller",
+        director: [
+            {
+                id: 1,
+                name: "Park Chan-wook",
+                bio: "...",
+                yearBirth: 1963,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
+    },
+    {
+        id: 2,
         title: 'Taxi Driver',
-        director: 'Martin Scorsese'
+        description: "...",
+        genre: "thriller",
+        director: [
+            {
+                id: 2,
+                name: "Martin Scorsese",
+                bio: "...",
+                yearBirth: 1942,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Oldboy',
-        director: 'Park Chan-wook'
+        id: 3,
+        title: "Léon: The Professional",
+        description: "...",
+        genre: "action-thriller",
+        director: [
+            {
+                id: 3,
+                name: "Luc Besson",
+                bio: "...",
+                yearBirth: 1959,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Léon: The Professional',
-        director: 'Luc Besson'
+        id: 4,
+        title: "Big Fish",
+        description: "...",
+        genre: "comedy-drama",
+        director: [
+            {
+                id: 4,
+                name: "Tim Burton",
+                bio: "...",
+                yearBirth: 1958,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Big Fish',
-        director: 'Tim Burton'
+        id: 5,
+        title: "The Last Samurai",
+        description: "...",
+        genre: "historical drama",
+        director: [
+            {
+                id: 5,
+                name: "Edward Zwick",
+                bio: "...",
+                yearBirth: 1952,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'The Last Samurai',
-        director: 'Edward Zwick'
+        id: 6,
+        title: "Schindler\'s List",
+        description: "...",
+        genre: "historical drama",
+        director: [
+            {
+                id: 6,
+                name: "Steven Spielberg",
+                bio: "...",
+                yearBirth: 1946,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Schindler\'s List',
-        director: 'Steven Spielberg'
+        id: 7,
+        title: "Full Metal Jacket",
+        description: "...",
+        genre: "war",
+        director: [
+            {
+                id: 7,
+                name: "Stanley Kubrick",
+                bio: "...",
+                yearBirth: 1928,
+                yearDeath: 1999
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Full Metal Jacket',
-        director: 'Stanley Kubrick'
+        id: 8,
+        title: "The New World",
+        description: "...",
+        genre: "drama",
+        director: [
+            {
+                id: 8,
+                name: "Terrence Malick",
+                bio: "...",
+                yearBirth: 1943,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'The New World',
-        director: 'Terrence Malick'
+        id: 9,
+        title: "Grand Budapest Hotel",
+        description: "...",
+        genre: "comedy-drama",
+        director: [
+            {
+                id: 9,
+                name: "Wes Anderson",
+                bio: "...",
+                yearBirth: 1969,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     },
     {
-        title: 'Grand Budapest Hotel',
-        director: 'Wes Anderson'
-    },
-    {
-        title: 'This Is England',
-        director: 'Shane Meadows'
+        id: 10,
+        title: "This Is England",
+        description: "...",
+        genre: "drama",
+        director: [
+            {
+                id: 10,
+                name: "Shane Meadows",
+                bio: "...",
+                yearBirth: 1972,
+                yearDeath: null
+            }
+        ],
+        imageURL: "..."
     }
 ];
 
