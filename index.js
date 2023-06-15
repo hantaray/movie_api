327678de07f3ada572a758b25a6114327530f55c
hansashi <ahantke@mail.de>
2023-06-15 13:19:53 +0200

    added allowedOrigin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
 app.use(cors({
     origin: (origin, callback) => {
