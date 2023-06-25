ce45398042e964118e39688c114c6f71ffdffcaf
hansashi <ahantke@mail.de>
2023-06-25 16:03:18 +0200

    updated cors

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movie-api-zy6n.onrender.com/movies'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movie-api-zy6n.onrender.com'];
 
-app.use(cors());
+
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
