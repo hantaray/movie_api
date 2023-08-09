488829ca0f77b192778b67f5d351ad4fc34b1a13
hansashi <ahantke@mail.de>
2023-08-09 17:57:36 +0200

    changed update userdata

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
     }
 
     let newPassword = "";
-    let user = Users.findOne({ username: req.params.Username });
+    let user = Users.findOne({ username: req.params.Username }, password);
     console.log('user', user)
 
     // check if newPassword is hashedPassword (hashedPassword was passed)
